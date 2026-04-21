9ca6a220231d525becd54ddf84b22a83bb98fd96
Leonardo <leo.sales.br@gmail.com>
2026-04-21 17:50:06 -0300

    serializers views  e urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from genres.views import GenreListCreateView, GenreRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path("genres/", GenreListCreateView.as_view(), name="genre_create_list"),
+    path("genres/<int:pk>/", GenreRetrieveUpdateDestroyView.as_view(), name="genre_detail_view"),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from genres.models import Genre
+from genres.serializers import GenreSerializer
 
-# Create your views here.
+
+class GenreListCreateView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+
+
