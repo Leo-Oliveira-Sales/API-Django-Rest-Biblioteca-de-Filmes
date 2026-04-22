eeb98a6060c730bbfd7361b6ee239e6d2b11fe1a
Leonardo <leo.sales.br@gmail.com>
2026-04-22 13:32:59 -0300

    CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreListCreateView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorListCreateView, ActorRetrieveUpdateDestroyView
 
 
 # Criando as urls para as views de CRUD dos gêneros
@@ -9,4 +10,7 @@ urlpatterns = [
 
     path("genres/", GenreListCreateView.as_view(), name="genre_create_list"),
     path("genres/<int:pk>/", GenreRetrieveUpdateDestroyView.as_view(), name="genre_detail_view"),
+
+    path("actors/", ActorListCreateView.as_view(), name="actor_create_list"),
+    path("actors/<int:pk>/", ActorRetrieveUpdateDestroyView.as_view(), name="actor_detail_view"),
 ]
