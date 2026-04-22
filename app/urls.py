5485d7745804da587af16c5a5cec0431e34e6a5f
Leonardo <leo.sales.br@gmail.com>
2026-04-22 15:01:38 -0300

    CRUD view serializers url admin

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreListCreateView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorListCreateView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieListCreateAPIView, MovieRetrieveUpdateDestroyAPIView
 
 
 # Criando as urls para as views de CRUD dos gêneros
@@ -13,4 +14,7 @@ urlpatterns = [
 
     path("actors/", ActorListCreateView.as_view(), name="actor_create_list"),
     path("actors/<int:pk>/", ActorRetrieveUpdateDestroyView.as_view(), name="actor_detail_view"),
+
+    path("movies/", MovieListCreateAPIView.as_view(), name="movie_create_list"),
+    path("movies/<int:pk>/", MovieRetrieveUpdateDestroyAPIView.as_view(), name="movie_detail_view"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from movies.models import Movie
+from movies.serializers import MovieSerializer
 
-# Create your views here.
+
+class MovieListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
